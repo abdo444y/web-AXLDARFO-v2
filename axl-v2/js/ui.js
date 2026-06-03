07cf5569e517e7a372658a4126a8039eb19e66fb
abdelrahman <abdoomar20072007@gmail.com>
2026-06-03 10:11:19 +0200

    Delete axl-v2 directory

--- axl-v2/js/ui.js
+++
@@ -1,9 +0,0 @@
-const UI = {
-    init: function() {
-        console.log("UI Initialized");
-        this.setupEventListeners();
-    },
-    setupEventListeners: function() {
-        // إضافة تفاعلات للأزرار هنا
-    }
-};
