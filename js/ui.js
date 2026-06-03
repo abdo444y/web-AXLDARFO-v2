3c5c01b3678d40ee1e05a4d01a01cc0af697dfac
abdelrahman <abdoomar20072007@gmail.com>
2026-06-03 10:12:30 +0200

    Add files via upload

---
+++ js/ui.js
@@ -0,0 +1,9 @@
+const UI = {
+    init: function() {
+        console.log("UI Initialized");
+        this.setupEventListeners();
+    },
+    setupEventListeners: function() {
+        // إضافة تفاعلات للأزرار هنا
+    }
+};
